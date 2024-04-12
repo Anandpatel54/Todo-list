998c1ed62f0087905a7001ce13f5bfa6bd7d9720
Anand patel <panand054@gmail.com>
2024-04-12 12:15:53 +0530

    Create, Rander,mark,completed, counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,58 @@
 import { useState } from "react";
 
 const App = () => {
+  const [title, setTitle] = useState("");
   const [tasks, setTasks] = useState([
-    { title: "Task 1", completed: false },
-    { title: "Task 2", completed: true },
-
+  //  { title: "Task 1", completed: false },
+ //   { title: "Task 2", completed: false },
   ]);
 
-  const completeTaskToggle = (e, i) =>{
-   e.target.classList.toggle("bg-green-500")
-   e.target.classList.toggle("border")
-   e.target.nextSibling.classList.toggle("line-through")
-   
-  }
+  const TaskSubmitHandler = (e) => {
+    e.preventDefault();
+    const copyTasks = [...tasks];
+    copyTasks.push({ title, completed: false });
+    setTasks(copyTasks);
+    setTitle("");
+  };
+
+  const completeTaskToggle = (e, i) => {
+    e.target.classList.toggle("bg-green-500");
+    e.target.classList.toggle("border");
+    e.target.nextSibling.classList.toggle("line-through");
+
+    const copyTask = [...tasks];
+    copyTask[i].completed = !tasks[i].completed;
+    setTasks(copyTask);
+  };
 
   let tasksrander = (
-    <h1 className="text-center font-extrabold text-2xl text-orange-500">No pending Task....</h1>
-  )
-  if(tasks.length > 0){
-    tasksrander = tasks.map((task, index)=>{
-      return(
-        <li key={index} className="mb-5 flex justify-between items-center border rounded-xl p-5">
-        <div className="flex items-center">
-          <div onClick={(e) => completeTaskToggle(e, index)} className="cursor-pointer mr-4 rounded-full w-[30px] h-[30px] border border-orange-500"></div>
-          <h1 className="cursor-pointer text-2xl font-extrabold text-yellow-100">
-            {task.title}
-          </h1>
-        </div>
-        <div className="flex gap-3 text-2xl text-yellow-100">
-          <i className="cursor-pointer ri-file-edit-line"></i>
-          <i className="cursor-pointer ri-delete-bin-3-line"></i>
-        </div>
-      </li>
-      
-      )
-
-    })
+    <h1 className="text-center font-extrabold text-2xl text-orange-500">
+      No pending Task....
+    </h1>
+  );
+  if (tasks.length > 0) {
+    tasksrander = tasks.map((task, index) => {
+      return (
+        <li
+          key={index}
+          className="mb-5 flex justify-between items-center border rounded-xl p-5"
+        >
+          <div className="flex items-center">
+            <div
+              onClick={(e) => completeTaskToggle(e, index)}
+              className="cursor-pointer mr-4 rounded-full w-[30px] h-[30px] border border-orange-500"
+            ></div>
+            <h1 className="cursor-pointer text-2xl font-extrabold text-yellow-100">
+              {task.title}
+            </h1>
+          </div>
+          <div className="flex gap-3 text-2xl text-yellow-100">
+            <i className="cursor-pointer ri-file-edit-line"></i>
+            <i className="cursor-pointer ri-delete-bin-3-line"></i>
+          </div>
+        </li>
+      );
+    });
   }
 
   return (
@@ -46,22 +63,27 @@ const App = () => {
           <p>Keeps doing things</p>
         </div>
         <div className="text-4xl font- extrabold w-[150px] h-[150px] bg-orange-600 rounded-full flex items-center justify-center">
-          1/3
+          {tasks.filter((t) => t.completed === true).length}/{tasks.length}
         </div>
       </div>
       {/*  */}
-      <form className="w-[35%] flex justify-between px-5 my-[2%]">
+      <form
+        onSubmit={TaskSubmitHandler}
+        className="w-[35%] flex justify-between px-5 my-[2%]"
+      >
         <input
           className="px-5 py-3 text-yellow-100 outline-none w-[85%] rounded-xl bg-zinc-700"
           type="text"
           placeholder="write your next task"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
         />
         <button className="outline-none text-4xl  font-extrabold flex justify-center items-center w-[50px] h-[50px] rounded-full bg-orange-600">
           <i className="ri-add-fill"></i>
         </button>
       </form>
       {/*  */}
-      <ul className="list-none w-[35%]">{tasksrander}</ul> 
+      <ul className="list-none w-[35%]">{tasksrander}</ul>
     </div>
   );
 };
